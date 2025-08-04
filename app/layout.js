f52966da625e1d05ac8ccd9e6ed2417f9d04f495
ssohail <sharjeelsoh@gmail.com>
2025-08-04 18:14:27 +1000

    first-draft

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,28 @@
 // src/app/layout.js
 import './global.css';
+import { Newsreader } from 'next/font/google';
+
+const newsreader = Newsreader({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['300', '400', '500', '600'], // Choose the weights you want
+});
 
 export const metadata = {
-  title: 'wedding invite',
-  description: 'A cute digital invite 💌',
+  title: 'our wedding',
+  description: 'you are invited',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={newsreader.className}>
+      <body>
+        <div className="main-wrapper">
+          <div className="content">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -2,6 +2,22 @@
 
 export default function Home() {
   return (
-    <h1>hello gorgeous batisha</h1>
+    <div className="invite-wrapper">
+      <div className="gif-wrapper">
+        <img src="/assets/sajistick.gif" alt="saji" />
+      </div>
+
+      <div className="invite-content">
+        <h2 className="m-0 p-0">sharjeel & isya</h2>
+        <p>two buddies are getting married</p>
+        <p>you're invited to our wedding</p>
+        <p>friday, september 12th, 2025</p>
+        <p>location: tanah jambu</p>
+      </div>
+
+      <div className="gif-wrapper">
+        <img src="/assets/isyastick.gif" alt="isya" />
+      </div>
+    </div>
   );
 }
