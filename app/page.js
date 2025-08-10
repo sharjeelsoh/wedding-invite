4c692fa498870821a873c7c19c6b5bdefac7162a
ssohail <sharjeelsoh@gmail.com>
2025-08-10 16:39:31 +1000

    fixes 2.0

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 'use client';
 
 import { useRouter } from "next/navigation";
-import { FaLongArrowAltUp } from "react-icons/fa";
 import { TfiAlarmClock } from "react-icons/tfi";
 import { MdOutlineLocationOn } from "react-icons/md";
 import { BsCalendarHeart } from "react-icons/bs";
@@ -12,25 +11,28 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="invite-wrapper">
+    <div>
+      <div className="invite-wrapper">
 
-      <div className="gif-wrapper">
-        <img onClick={() => router.push("/saji")} src="/assets/sajistick.gif" alt="saji" />
-        <p>click me <FaLongArrowAltUp /></p>
+        <div className="gif-wrapper">
+          <img onClick={() => router.push("/saji")} src="/assets/sajistick.gif" alt="saji" />
+        </div>
+
+        <div className="invite-content">
+          <h2 className="m-0 p-0">sharjeel & isya</h2>
+          <p>two buddies are getting married</p><br></br>
+          <p><BsCalendarHeart /> friday, september 12th, 2025</p>
+          <p><TfiAlarmClock /> 19:00 - 22:00 bnt</p>
+          <p><MdOutlineLocationOn /> mahligai al-barakah, salambigar, brunei.</p>
+        </div>
+
+        <div className="gif-wrapper">
+          <img onClick={() => router.push("/isya")} src="/assets/isyastick.gif" alt="isya" />
+        </div>
+
       </div>
 
-      <div className="invite-content">
-        <h2 className="m-0 p-0">sharjeel & isya</h2>
-        <p>two buddies are getting married</p><br></br>
-        <p><BsCalendarHeart /> friday, september 12th, 2025</p>
-        <p><TfiAlarmClock /> 19:00 - 22:00 bnt</p>
-        <p><MdOutlineLocationOn /> mahligai al-barakah, salambigar, brunei.</p>
-      </div>
-
-      <div className="gif-wrapper">
-        <img onClick={() => router.push("/isya")} src="/assets/isyastick.gif" alt="isya" />
-        <p>click me <FaLongArrowAltUp/></p>
-      </div> 
+      <p className="footer"><em>(click us to find more)</em></p>
     </div>
   );
 }
